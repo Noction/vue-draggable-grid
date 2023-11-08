44e97acca75d498d4985409f4f71045cb15cda39
lwvemike <plamadeala.mihai002@gmail.com>
2023-11-08 11:22:32 +0200

    fix(types): add ts-ignore, so will not block future github-actions

--- packages/vue-draggable-grid/vitest.config.ts
+++ packages/vue-draggable-grid/vitest.config.ts
@@ -1,17 +1,17 @@
-import * as path from 'path'
 import { defineConfig } from 'vitest/config'
+import { resolve }  from 'node:path'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': resolve(__dirname, './src')
     }
   },
   test: {
     environment: 'jsdom',
     globals: true,
-    include: ['tests/*.spec.ts', 'tests/unit/*.spec.ts']
+    include: ['test/*.spec.ts']
   }
 })
